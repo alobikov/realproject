6559de6f9253fb35e1b087d511fb793e0fb53391
Aleksej Lobikov <aleksej.lobikov@gmail.com>
2020-07-06 22:38:50 +0300

    stage 1 complete

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+console.log(1);
+import "./slider";
+import modal from "./modules/modal";
+
+document.addEventListener("DOMContentLoaded", () => {
+  modal();
+});
---
+++ src/js/modules/modal.js
@@ -0,0 +1,43 @@
+console.log("modal() there");
+function modal() {
+  function bindModal(triggerSelector, modalSelector, closeSelector) {
+    const trigger = document.querySelectorAll(triggerSelector),
+      modal = document.querySelector(modalSelector),
+      close = document.querySelector(closeSelector);
+
+    trigger.forEach((trig) => {
+      trig.addEventListener("click", (e) => {
+        if (e.target) e.preventDefault();
+        modal.style.display = "block";
+        document.body.style.overflow = "hidden";
+      });
+    });
+    close.addEventListener("click", (e) => {
+      modal.style.display = "none";
+      document.body.style.overflow = "";
+    });
+    // close event handler when clicked outside modal
+    modal.addEventListener("click", (e) => {
+      console.log(e.target);
+      console.log(modal);
+      if (modal === e.target) {
+        modal.style.display = "none";
+        document.body.style.overflow = "";
+      }
+    });
+  }
+  function showModalOnTimeout(selector, delay) {
+    setTimeout(() => {
+      document.querySelector(selector).style.display = "block";
+      document.body.style.overflow = "hidden";
+    }, delay);
+  }
+  bindModal(
+    ".popup_engineer_btn",
+    ".popup_engineer",
+    ".popup_engineer .popup_close"
+  );
+  bindModal(".phone_link", ".popup", ".popup .popup_close");
+  showModalOnTimeout(".popup", 3000);
+}
+export default modal;
